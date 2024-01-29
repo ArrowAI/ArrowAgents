59088b02725ea89a55f371ab723f4222a1da99ad
Utkarsh Shukla <utk.shukla@gmail.com>
2024-01-29 18:43:25 +0530

    change

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
 import { CronService } from "./services/cron";
 import { Engine } from "./engine/engine";
+import express from 'express';
+import { router } from "./routes/routes";
 
 const engine = new Engine();
 const cron = new CronService(engine);
 cron.start()
 
-//here you can also start the express service so that it can start functions like forced run to test or static trigger instead of automatic trigger.
+
+const app = express();
+
+app.use(router);
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
