5442a79bd492ed7f0e515f2a51c2012bbef15774
ravi18 <ravi@arrowai.com>
2024-01-30 15:35:56 +0530

    changes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,26 +3,35 @@ const router = express.Router();
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+import { Engine } from './../engine/engine';
+const engine = new Engine();
 const SERVER_MODE = process.env.SERVER_MODE || "master";
 
-if(SERVER_MODE == "master"){
+if (SERVER_MODE == "master") {
     router.get('/', (req, res) => {
         res.send('Hello World from master!');
     });
     router.get('/registerslave', (req, res) => {
         res.send('Hello slave!');
     });
-    router.get('/registerfunctionexecutor',(req,res)=>{
+    router.get('/registerfunctionexecutor', (req, res) => {
 
     });
 }
-else{
+else {
     router.get('/', (req, res) => {
         res.send('Hello World from slave!');
     });
-    
+
+
+
 }
 
-    
+router.get('/run/:flowId', (req, res) => {
+    let response = engine.execute(req.params.flowId);
+    res.json(response);
+});
+
+
 
 export { router };
