e4bf076a426718580872e9915f6204edd219f413
Utkarsh Shukla <utk.shukla@gmail.com>
2024-01-31 12:10:24 +0530

    changes

--- test/iterateGraph.test.ts
+++ test/iterateGraph.test.ts
@@ -1,75 +1,8 @@
 import {FlowExecuteHandler} from "../src/execution/flowexecute"
 
 
-var FlowJson = {
-    "name": "Example Workflow",
-    "nodes": [
-        {
-            "id": "node0",
-            "type": "control",
-            actions: [{
-                actionId: "action0",
-                actionType: "node0.action",
-                actionProperties: {
-                    name: "action0",
-                    description: "action0",
-                }
-            }]
-        },
-        {
-            "id": "node1",
-            "type": "control",
-            actions: [{
-                actionId: "action0",
-                actionType: "node1.action",
-                actionProperties: {
-                    name: "action0",
-                    description: "action0",
-
-                }
-            }]
-        },
-        {
-            "id": "node2",
-            "type": "data",
-            actions: [{
-                actionId: "action0",
-                actionType: "node2.action",
-                actionProperties: {
-                    name: "action0",
-                    description: "action0",
-
-                }
-            }]
-        },
-        {
-            "id": "node3",
-            "type": "data",
-            actions: [{
-                actionId: "action0",
-                actionType: "node3.action",
-                actionProperties: {
-                    name: "action0",
-                    description: "action0",
-                }
-            }]
-        }
-    ],
-    "connections": [
-        {
-            "source": "node2",
-            "target": "node0"
-        },
-        {
-            "source": "node3",
-            "target": "node1"
-        },
-        {
-            "source": "node0",
-            "target": "node3"
-        }
-    ]
-}
+// Open actualworkflow.json file and parse the json as variable workflow
+var workflow = require('../datajson/actualworkflow.json');
 
 describe('Graph Iteration', () => {
     it('Execute the flow', async () => {
